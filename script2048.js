14fa5d546cbef711f8715a1da61e5e4227d9adfb
Verbrugge <louvbg@gmail.com>
2024-02-20 23:19:38 +0100

    bientot fini transition

--- script2048.js
+++ script2048.js
@@ -4,8 +4,8 @@ const HEIGHT = 800;
 const WIDTH = 800;
 
 
-let nbCaseX = 4;   
-let nbCaseY = 4;    
+let nbCaseX = 4;
+let nbCaseY = 4;
 let tailleChiffre = WIDTH / nbCaseX / 3;
 let tailleCase = WIDTH / nbCaseX;
 let tailleBordure = tailleCase / 10;
@@ -14,17 +14,18 @@ let tailleBordure = tailleCase / 10;
 //cadre de jeu:
 let debutPlateauX = tailleBordure;
 let debutPlateauY = tailleBordure;
-let largeurPlateau = WIDTH - tailleBordure - debutPlateauX;
+let largeurPlateau = WIDTH - debutPlateauX;
 let hauteurPlateau = HEIGHT - tailleBordure - debutPlateauY;
 
 //plateau de jeu:
 let plateau;
 let listeCaseMouvement = [
-    [{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 }],
-    [{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 }],
-    [{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 }],
-    [{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 },{ value: 0, animation: false, targetX:0, targetY:0 }]
+    [{ valeurAnnimation: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }],
+    [{ value: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }],
+    [{ value: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }],
+    [{ value: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }, { value: 0, animation: false, targetX: 0, targetY: 0 }]
 ];
+let creationNewCase = false;
 
 let couleurCase = {
     0: "#ccc0b2",
@@ -32,7 +33,7 @@ let couleurCase = {
     4: "#eddfc4",
     8: "#f4b17a",
     16: "#f59563",
-    32: "#f67e5b",  
+    32: "#f67e5b",
     64: "#f65e39",
     128: "#edce73",
     256: "#edca64",
@@ -48,7 +49,7 @@ window.onload = function () {
     board.height = HEIGHT;
     board.width = WIDTH;
     context = board.getContext("2d");
-    
+
     requestAnimationFrame(game);
     document.addEventListener("keydown", pressKey)
     initGame();
@@ -69,13 +70,11 @@ function pressKey(event) {
     } else if (event.key == "d") {
         deplacementCase("droite");
         deplacement = true;
-    } 
-    if (deplacement) {
-        ajouterNouvelleCase();
-        //affichePlateau(); // sans annimation
-        affichePlat();      // avec annimation
     }
-    
+    if (deplacement) {
+        annimationCase();      // avec annimation
+    }
+
 }
 
 function initGame() {
@@ -84,7 +83,7 @@ function initGame() {
     for (let y = 0; y < nbCaseY; y++) {
         listeCaseMouvement.push([])
         for (let x = 0; x < nbCaseX; x++) {
-            listeCaseMouvement[y].push({ value: 0, animation: false, x: x*tailleCase, y:y*tailleCase, ancienneX: x*tailleCase, ancienneY:y*tailleCase });
+            listeCaseMouvement[y].push({ value: 0, animation: false, x: x * tailleCase, y: y * tailleCase, ancienneX: x * tailleCase, ancienneY: y * tailleCase });
         }
     }
     ////
@@ -95,15 +94,19 @@ function initGame() {
             plateau[y].push(0)
         }
     }
-    ajouterNouvelleCase();
-    ajouterNouvelleCase();
+    //ajouterNouvelleCase();
+    //ajouterNouvelleCase();
+    listeCaseMouvement[0][0].value = 2;
+    listeCaseMouvement[0][3].value = 2;
+    listeCaseMouvement[1][2].value = 4;
+
+
     affichePlateau();
 
 }
 
 function game() {
 
-    //affichePlateau()
 
     if (!finGame()) {
         requestAnimationFrame(game)
@@ -131,47 +134,61 @@ function finGame() {
 }
 
 
-function affichePlat() {
+function annimationCase() {
+    creationNewCase = false;
     listeCaseMouvement.forEach(function (element) {
         element.forEach(function (elem) {
-            if ( elem.animation ) {
-                gsap.fromTo(elem, {x: elem.x, y : elem.y }, { x: elem.x+elem.targetX, y: elem.y+elem.targetY, duration: 2,
-                                onUpdate: function() {
-                                    drawSquare(elem);
-                                }, onComplete: () => {
-                                    // on remet les valeurs à 0:
-                                    listeCaseMouvement[(elem.y/tailleCase)][(elem.x/tailleCase)].value = elem.value;
-                                    listeCaseMouvement[(elem.y/tailleCase)-(elem.targetY/tailleCase)][(elem.x/tailleCase)-(elem.targetX/tailleCase)].value = 0;
-                                    listeCaseMouvement[(elem.y/tailleCase)-(elem.targetY/tailleCase)][(elem.x/tailleCase)-(elem.targetX/tailleCase)].animation = false;
-                                    elem.x = elem.ancienneX;
-                                    elem.y = elem.ancienneY;
-                                    elem.targetY = 0;
-                                    elem.targetX = 0;
-                                } })
+            if (elem.animation && (elem.targetX != 0 || elem.targetY != 0)) {
+                gsap.fromTo(elem, { x: elem.x, y: elem.y }, {
+                    x: elem.x + elem.targetX, y: elem.y + elem.targetY, duration: 1,
+                    onUpdate: function () {
+                        affichePlateau();
+                    }, onComplete: () => {
+                        // on remet les valeurs à 0:
+                        listeCaseMouvement[(elem.y / tailleCase)][(elem.x / tailleCase)].value = elem.value;
+                        listeCaseMouvement[(elem.y / tailleCase) - (elem.targetY / tailleCase)][(elem.x / tailleCase) - (elem.targetX / tailleCase)].value = 0;
+                        listeCaseMouvement[(elem.y / tailleCase) - (elem.targetY / tailleCase)][(elem.x / tailleCase) - (elem.targetX / tailleCase)].animation = false;
+                        elem.x = elem.ancienneX;
+                        elem.y = elem.ancienneY;
+                        elem.targetY = 0;
+                        elem.targetX = 0;
+                        if (!creationNewCase) {
+                            ajouterNouvelleCase();
+                            creationNewCase = true;
+                        }
+                    }
+                })
             }
         })
     })
 }
 
-function drawSquare(elem) {
-    
-    //affiche carre:
-    context.fillStyle = couleurCase[elem.value];
-    context.fillRect(elem.x, elem.y, largeurPlateau / nbCaseX, hauteurPlateau / nbCaseY);
-
-    //affiche texte:
-    context.font = tailleChiffre+"px verdana";
-    context.textAlign = "center";
-    context.textBaseline = "middle";
-    context.fillStyle = "#776e65";
-    context.fillText(elem.value, elem.x+tailleCase/2, elem.y+tailleCase/2);
-
-    affichePlateau() 
-}
-
-
 function affichePlateau() {
- 
+
+    context.fillStyle = "black";
+    context.clearRect(0, 0, WIDTH, HEIGHT);
+
+    listeCaseMouvement.forEach(function (element) {
+
+        element.forEach(function (elem) {
+
+            if (elem.value != 0) {
+                //affiche carre:
+                context.fillStyle = couleurCase[elem.value];
+                context.fillRect(elem.x, elem.y, largeurPlateau / nbCaseX, hauteurPlateau / nbCaseY);
+
+                //affiche texte:
+                context.font = tailleChiffre + "px verdana";
+                context.textAlign = "center";
+                context.textBaseline = "middle";
+                context.fillStyle = "#776e65";
+                context.fillText(elem.value, elem.x + tailleCase / 2, elem.y + tailleCase / 2);
+            }
+        })
+
+
+    });
+
     //affiche bordure:
     context.strokeStyle = "#bbaea0";
     context.lineWidth = tailleBordure;
@@ -183,15 +200,16 @@ function affichePlateau() {
 }
 
 
+
 function ajouterNouvelleCase() {
     // nombre aleatoire entre 0-3, si c'est 0 je fais apparaitre une case 4
     // sinon je fais apparaiter une case 2
     let nombreAlea = Math.floor(Math.random() * 4);
     let valeurNewCase = 0;
     if (nombreAlea == 0) {
-        valeurNewCase=4;
+        valeurNewCase = 4;
     } else {
-        valeurNewCase=2;
+        valeurNewCase = 2;
     }
 
     let trouvePlace = false;
@@ -203,7 +221,7 @@ function ajouterNouvelleCase() {
             trouvePlace = true;
         }
     }
-    
+
 }
 
 
@@ -224,7 +242,7 @@ function deplacementCase(direction) {
             arrayModif.push([]);
             for (let y = 0; y < nbCaseY; y++) {
                 arrayModif[x].push(listeCaseMouvement[y][x].value);
-                
+
 
             }
         }
@@ -232,13 +250,13 @@ function deplacementCase(direction) {
     else if (direction == "haut") {
         for (let x = 0; x < nbCaseX; x++) {
             arrayModif.push([]);
-            for (let y = 1; y < nbCaseY+1; y++) {
-                arrayModif[x].push(listeCaseMouvement[nbCaseY-y][x].value);
-                
+            for (let y = 1; y < nbCaseY + 1; y++) {
+                arrayModif[x].push(listeCaseMouvement[nbCaseY - y][x].value);
+
 
             }
         }
-    }    else if (direction == "droite") {
+    } else if (direction == "droite") {
         for (let y = 0; y < nbCaseY; y++) {
             arrayModif.push([])
             for (let x = 0; x < nbCaseX; x++) {
@@ -249,7 +267,7 @@ function deplacementCase(direction) {
         for (let y = 0; y < nbCaseY; y++) {
             arrayModif.push([])
             for (let x = 1; x < nbCaseX + 1; x++) {
-                arrayModif[y].push(listeCaseMouvement[y][nbCaseX-x].value)
+                arrayModif[y].push(listeCaseMouvement[y][nbCaseX - x].value)
             }
         }
     }
@@ -258,43 +276,100 @@ function deplacementCase(direction) {
     //on supprime les 0:
     let mouvementCase = []
     let nb0aSupprimer = 0;
-    let decallage = 0;
     for (let y = 0; y < nbCaseY; y++) {
         mouvementCase.push([]);
         for (let x = 1; x < nbCaseX + 1; x++) {
 
-            if (arrayModif[y][nbCaseX -x +decallage] == 0) {
-                arrayModif[y].splice(nbCaseX - x+decallage, 1);
-                arrayModif[y].unshift(0);
-                nb0aSupprimer ++;
-                decallage++;
-                mouvementCase[y].push(0);
+            if (arrayModif[y][nbCaseX - x] == 0) {
+                arrayModif[y].splice(nbCaseX - x, 1);
+                nb0aSupprimer++;
             }
-            else if (arrayModif[y][nbCaseX - x+decallage] != 0) {
-                mouvementCase[y].push(nb0aSupprimer*tailleCase) 
+            else {
+                mouvementCase[y].push(nb0aSupprimer * tailleCase)
             }
         }
-        console.log("test")
         nb0aSupprimer = 0;
-        decallage = 0;
-
     }
+
+    //Etape 2.3:
+    //on additionne les chiffres si c'est possible:
+
+    console.log("FALITY !!!")
+    let nbCaseListeX;
+
+    for (let y = 0; y < mouvementCase.length; y++) {
+        nbCaseListeX = mouvementCase[y].length;
+        for (let x = 2; x < nbCaseListeX + 1; x++) {
+            if (arrayModif[y][nbCaseListeX - x] == arrayModif[y][nbCaseListeX - x + 1] || arrayModif[y][nbCaseListeX - x + 1] == 0) {
+                mouvementCase[y].reverse();
+                mouvementCase[y][nbCaseListeX - x] += tailleCase;
+                mouvementCase[y].reverse();
+
+                arrayModif[y][nbCaseListeX - x] = 0
+            }
+        }
+    }
+
+
+    /*
+    Il faut regler le pb pour remettre les infos dans la grille !!!
+    */
+
 
     //Etape 3.2:
     //je replace les chiffres dans la grille:
     if (direction == "droite") {
-        for (let y = 0; y<mouvementCase.length; y++) {
+        for (let y = 0; y < listeCaseMouvement.length; y++) {
             mouvementCase[y].reverse();
-            for (let x = 0; x < mouvementCase[y].length; x++) {
-                if (mouvementCase[y][x] != 0) {
-                    listeCaseMouvement[y][x].targetX = mouvementCase[y][x];
+            for (let x = 0; x < listeCaseMouvement[y].length; x++) {
+                if (listeCaseMouvement[y][x].value != 0) {
+                    listeCaseMouvement[y][x].targetX = mouvementCase[y][0];
                     listeCaseMouvement[y][x].targetY = 0;
                     listeCaseMouvement[y][x].animation = true;
+                    mouvementCase[y].splice(0, 1);
                 }
             }
         }
     } else if (direction == "gauche") {
-        for (let y = 0; y<mouvementCase.length; y++) {
+        for (let y = 0; y < listeCaseMouvement.length; y++) {
+            for (let x = 0; x < listeCaseMouvement[y].length; x++) {
+                if (listeCaseMouvement[y][x].value != 0) {
+                    listeCaseMouvement[y][x].targetX = -mouvementCase[y][0];
+                    listeCaseMouvement[y][x].targetY = 0;
+                    listeCaseMouvement[y][x].animation = true;
+                    mouvementCase[y].splice(0, 1);
+                }
+            }
+        }
+    } else if (direction == "haut") {
+        for (let x = 0; x < listeCaseMouvement.length; x++) {
+            for (let y = 0; y < listeCaseMouvement[x].length; y++) {
+                if (listeCaseMouvement[y][x].value != 0) {
+                    listeCaseMouvement[y][x].targetX = 0;
+                    listeCaseMouvement[y][x].targetY = -mouvementCase[x][0];
+                    listeCaseMouvement[y][x].animation = true;
+                    mouvementCase[x].splice(0, 1);
+                }
+            }
+        }
+    } else if (direction == "bas") {
+        for (let y = 0; y < listeCaseMouvement.length; y++) {
+            mouvementCase[y].reverse();
+
+            for (let x = 0; x < listeCaseMouvement[y].length; x++) {
+
+                if (listeCaseMouvement[x][y].value != 0) {
+                    listeCaseMouvement[x][y].targetY = mouvementCase[y][0];
+                    listeCaseMouvement[x][y].targetX = 0;
+                    listeCaseMouvement[x][y].animation = true;
+                    mouvementCase[y].splice(0, 1);
+                }
+            }
+        }
+    }
+
+    /*else if (direction == "gauche") {
+        for (let y = 0; y < mouvementCase.length; y++) {
             for (let x = 0; x < mouvementCase[y].length; x++) {
                 if (mouvementCase[y][x] != 0) {
                     listeCaseMouvement[y][x].targetX = -mouvementCase[y][x];
@@ -303,63 +378,33 @@ function deplacementCase(direction) {
                 }
             }
         }
-    }
-   
-
-    /*
-    //Etape 3:
-    let tailleListeX = arrayModif[0].length
-    let tailleListeY = arrayModif.length
-
-    for (let y = 0; y < tailleListeY; y++) {
-        for (let x = 2; x < tailleListeX+1; x++) {
-            if (arrayModif[y][tailleListeX - x] == arrayModif[y][tailleListeX - x + 1] || arrayModif[y][tailleListeX - x + 1] == 0 ) {
-                arrayModif[y][tailleListeX - x + 1] += arrayModif[y][tailleListeX - x]
-                arrayModif[y].splice(tailleListeX - x, 1);
-                arrayModif[y].unshift(0);
-            }
-        }
-    }
-
-    // Etape 4 replacer dans le grille du plateau:
-    if (direction == "haut") {
-        for (let x = 0; x < nbCaseX; x++) {
-            arrayModif[x].reverse()
-            for (let y = 0; y < nbCaseY; y++) {
-                listeCaseMouvement[y][x].ancienneX = x*tailleCase;
-                listeCaseMouvement[y][x].ancienneY = y*tailleCase;
-                listeCaseMouvement[y][x].value = arrayModif[x][y];
+    } else if (direction == "haut") {
+        for (let x = 0; x < mouvementCase.length; x++) {
+            for (let y = 0; y < mouvementCase[x].length; y++) {
+                if (mouvementCase[x][y] != 0) {
+                    listeCaseMouvement[y][x].targetY = -mouvementCase[x][y];
+                    listeCaseMouvement[y][x].targetX = 0;
+                    listeCaseMouvement[y][x].animation = true;
+                }
             }
         }
     } else if (direction == "bas") {
-        for (let x = 0; x < nbCaseX; x++) {
-            for (let y = 0; y < nbCaseY; y++) {
-                listeCaseMouvement[y][x].ancienneX = x*tailleCase;
-                listeCaseMouvement[y][x].ancienneY = y*tailleCase;
-                listeCaseMouvement[y][x].value = arrayModif[x][y]  // listeCaseMouvement[y][x].value
-            }
-        }
-    } else if (direction == "droite") {
-        for (let y = 0; y < nbCaseY; y++) {
-            for (let x = 0; x < nbCaseX; x++) {
-                listeCaseMouvement[y][x].ancienneX = x*tailleCase;
-                listeCaseMouvement[y][x].ancienneY = y*tailleCase;
-                listeCaseMouvement[y][x].value = arrayModif[y][x]
-            }
-        }
-    } else if (direction == "gauche") {
-        for (let y = 0; y < nbCaseY; y++) {
-            arrayModif[y].reverse()
-            for (let x = 0; x < nbCaseX; x++) {
-                listeCaseMouvement[y][x].ancienneX = x*tailleCase;
-                listeCaseMouvement[y][x].ancienneY = y*tailleCase;
-                listeCaseMouvement[y][x].value = arrayModif[y][x]
+        for (let y = 0; y < mouvementCase.length; y++) {
+            mouvementCase[y].reverse();
+            for (let x = 0; x < mouvementCase[y].length; x++) {
+
+                if (mouvementCase[y][x] != 0) {
+                    listeCaseMouvement[x][y].targetY = mouvementCase[y][x];
+                    listeCaseMouvement[x][y].targetX = 0;
+                    listeCaseMouvement[x][y].animation = true;
+
+                }
             }
         }
     }
     */
+
     console.log("test")
-    
 
 }
 
@@ -370,12 +415,8 @@ function restartGame() {
         y: -1000, duration: 0.5, ease: "step", onComplete: function () {
             initGame()
             gsap.to(".jeu2048", { y: 0, duration: 2, ease: "bounce" }); // Retour à la position initiale
-            
+
         }
     });
 }
-
-
-
-
 
